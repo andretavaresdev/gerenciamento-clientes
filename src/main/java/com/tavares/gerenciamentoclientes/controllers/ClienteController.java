4526c2164dc1edc462081734be5ffd42430164c8
Andre Tavares <andreetavaress1@gmail.com>
2026-03-23 21:50:04 -0300

    add: controller de Cliente criado

---
+++ src/main/java/com/tavares/gerenciamentoclientes/controllers/ClienteController.java
@@ -0,0 +1,38 @@
+package com.tavares.gerenciamentoclientes.controllers;
+
+import com.tavares.gerenciamentoclientes.models.Cliente;
+import com.tavares.gerenciamentoclientes.services.ClienteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    private ClienteService service;
+
+    @PostMapping
+    public ResponseEntity<Cliente> salvar(@RequestBody Cliente cliente){
+        return ResponseEntity.status(HttpStatus.CREATED).body(cliente);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarTodos(){
+        return ResponseEntity.ok(service.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id){
+        return service.buscarPorId(id)
+                .map(cliente -> ResponseEntity.ok(cliente))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Cliente> deletar (@PathVariable Long id){
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
